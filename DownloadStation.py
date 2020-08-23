fd9bcc431cf9312a9fa586d2a1e35e8071f74875
yymin1022 <yymin1022@gmail.com>
2020-08-23 17:23:22 +0900

    Update Download Algorithm : Reload List after Register Task

--- DownloadStation.py
+++ DownloadStation.py
@@ -131,5 +131,6 @@ class DownloadStation(QWidget):
                 response = self.curSession.post(url="%s/webapi/DownloadStation/task.cgi" %(self.synoURL),
                                                 data="api=SYNO.DownloadStation.Task&version=1&method=create&uri=%s" %(URL)).text
             print(response)
+            self.loadTaskList()
 
         self.inputUrl.clear()
